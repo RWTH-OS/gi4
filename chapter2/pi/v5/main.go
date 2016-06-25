3b71812d2c090db566e3b2f2e8c88d976ecca6e9
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-06-25 14:24:01 +0200

    a limit of coroutines

--- chapter2/pi/v5/main.go
+++ chapter2/pi/v5/main.go
@@ -10,20 +10,26 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"runtime"
 )
 
 var step float64
 
-func term(ch chan float64, i int) {
-	x := (float64(i) + 0.5) * step
-	x = 4.0 / (1.0 + x * x)
-	
-	ch <- x
+func term(ch chan float64, start, end int) {
+	var res float64
+
+	for i := start; i < end; i++ {
+		x := (float64(i) + 0.5) * step
+		res += 4.0 / (1.0 + x * x)
+	}
+
+	ch <- res
 }
 
 func main() {
 	var num_steps int
 	ch := make(chan float64)
+	max_goroutines := runtime.NumCPU()
 
 	if len(os.Args) > 1 {
 		num_steps, _ = strconv.Atoi(os.Args[1])
@@ -38,11 +44,14 @@ func main() {
 
 	start := time.Now()
 
-	for i := 0; i < num_steps; i++ {
-		go term(ch, i)
+	for i := 0; i < max_goroutines; i++ {
+		start := (num_steps / max_goroutines) * i
+		end := (num_steps / max_goroutines) * (i+1)
+
+		go term(ch, start, end)
 	}
 
-	for i := 0; i < num_steps; i++ {
+	for i := 0; i < max_goroutines; i++ {
 		sum += <-ch
 	}
 
