2186db3d6fa764cc4cd68b1cf3813cbb750d4ae7
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-05-20 17:16:25 +0200

    add CI test for the maximum example

--- chapter2/maximum/maximum.asm
+++ chapter2/maximum/maximum.asm
@@ -7,14 +7,14 @@ SECTION .text
 global maximum
 
 maximum:
-	push rbp		; save old rbp
+	push rbp	; save old rbp
 	mov rbp, rsp	; create new rbp
 
 	mov rax, rdi	; load 1st argument
 	cmp rdi, rsi	; compare both arguments
-	jg  L1			; jump greater to L1
+	jg  L1		; jump greater to L1
 	mov rax, rsi    ; if not then move 2nd argument to rax
 L1:
 
-	pop rbp			; restore rbp
-	ret				; jump back to addr before function was called
+	pop rbp		; restore rbp
+	ret		; jump back to addr before function was called
